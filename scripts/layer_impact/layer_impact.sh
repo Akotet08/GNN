65baa0b773de10c48ecc02b358f425c5a805e0b7
Akotet Y. Tesema <46914459+Akotet08@users.noreply.github.com>
2024-08-09 12:06:33 +0900

    Update script

--- scripts/layer_impact/layer_impact.sh
+++ scripts/layer_impact/layer_impact.sh
@@ -5,9 +5,9 @@ do
   do
     gpu=$(( (gpu + 1) % 8 ))
     layers=$((outer + idx))
-    python main.py --dataset movielense \
+    python main.py --dataset book_crossing \
                    --method lightgcn \
-                   --note layers_imapct_movielense_0806_1830 \
+                   --note layers_imapct_movielense_0807_1715 \
                    --hps \
                    --num_layers $layers \
                    --epochs 100 \
